39cf632b4731f6ffe4362aaf798b22f57c9ad76a
youssef3332-a11y <youssef17102003@gmail.com>
2024-08-13 11:21:40 +0100

    chore: Refactor FileStorage class to support filtering by class in `all` method

--- models/base_model.py
+++ models/base_model.py
@@ -2,25 +2,27 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+        self.id = str(uuid.uuid4())  # Generate a unique ID
+        self.created_at = datetime.now()  # Set the current time for creation
+        self.updated_at = datetime.now()  # Set the current time for update
+
+        # Handle additional attributes passed via kwargs
+        for key, value in kwargs.items():
+            setattr(self, key, value)  
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -31,6 +33,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)#added
         storage.save()
 
     def to_dict(self):
@@ -42,3 +45,8 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
+    
+    def delete(self):
+        """Delete the current instance from the storage"""
+        from models import storage
+        storage.delete(self)
